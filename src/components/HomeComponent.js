800be3c8823faa56a0205bc9b19ae6e2dd2b807a
aslam021 <muhammadh.aslamm@gmail.com>
2020-07-04 20:56:51 +0530

    Fetch Handling Errors

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -18,18 +18,18 @@ function RenderCard({item, isLoading, errMess}) {
                 <h4>{errMess}</h4>
         );
     }
-    else 
-        return(
-            <Card>
-                <CardImg src={baseUrl + item.image} alt={item.name} />
-                <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
-                <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
-        );
-
+    else {
+      return(
+        <Card>
+            <CardImg src={baseUrl + item.image} alt={item.name} />
+            <CardBody>
+            <CardTitle>{item.name}</CardTitle>
+            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+            <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+      );
+    }
 }
 
 function Home(props) {
